797b994132607f8679b2713eb01ebe9b1f4f6587
Landon Babay <landon.babay@outlook.com>
2023-06-14 13:22:46 -0400

    working on the about page

--- app/api/message/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = async function (context, req) {
-    console.log(context)
-    context.res.json({
-        text: "Hello from the API"
-    });
-}
--- app/src/pages/About.js
+++ app/src/pages/About.js
@@ -1,7 +1,31 @@
+import styled from 'styled-components'
+
 export const About = () => { 
+    const Wrapper = styled.div`
+        .hero-about { 
+            display: flex;
+            justify-content: center;
+            padding: 15px;
+            flex-direction: column;
+            align-items: center;
+            
+        }
+        .hero-content{
+            text-align: center;
+        }
+    `
     return(
-        <div>
-            about
-        </div>
+       <Wrapper>
+       <div className='hero-about'>
+           <h1>About Me</h1>
+           <div className='hero-content'>
+                <h4>I'm a DevOps Engineer currently focusing on Azure/AWS, CI/CD, containerization, cloud architecture and Design patterns, kubernetes, and full stack development.</h4>
+                <br></br>
+                <h4>I started my career in the systems administration space doing all on-premise work, but I have always had a fascination with the cloud and software. After moving on from sysadmin, I worked as a security engineer focused on cloud security, risk management, incident response, and IAM. Security engineering was interesting, but ultimately I still had an itch to get into cloud or devops engineering, so I moved onto a devops team where I got my feet wet and learned a ton. </h4>
+                <br/>
+                <h4>Some things that I enjoy outside of work are golf, pickleball, and working on personal projects</h4>
+           </div>
+       </div>
+       </Wrapper>
     )
 }
