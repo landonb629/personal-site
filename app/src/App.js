3f30273311a04458235e59f9021f832b7edfd526
Landon Babay <landon.babay@outlook.com>
2023-06-21 08:46:38 -0400

    adding the content component to conditionally render different content

--- app/src/App.js
+++ app/src/App.js
@@ -4,14 +4,13 @@ import {Nav} from './components/Nav'
 import {Posts} from './pages/Posts'
 import {Routes, Route} from 'react-router-dom'
 import styled from 'styled-components'
-import {Post} from './components/Post'
 import Header from './components/Header'
 import {RxHamburgerMenu} from 'react-icons/rx'
 import {useState} from 'react'
 import {FaAws} from 'react-icons/fa'
 import {SiKubernetes, SiMicrosoftazure, SiLinux} from 'react-icons/si'
 import {Projects} from './components/Projects'
-import {Project} from './pages/Project'
+import {Content} from './components/Content'
 
 
 function App() {
@@ -30,9 +29,9 @@ function App() {
       <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/posts" element={ <Posts />} />
-          <Route path="/posts/:id" exact={true} element={ <Post/>}/>
           <Route path="/projects" exact={true} element={ <Projects />} />
-          <Route path="/projects/:id" exact={true} element={ <Project  />} />
+          <Route path="/projects/:id" exact={true} element={ <Content/>} />
+          <Route path="/posts/:id" exact={true} element={ <Content/>}/>
       </Routes>
       </div>
       <footer>
--- app/src/components/Content.js
+++ app/src/components/Content.js
@@ -1,8 +1,61 @@
-import {useParams} from 'react-router-dom'
+import {useParams, useLocation} from 'react-router-dom'
+import {useState, useEffect} from 'react'
+import ReactMarkdown from 'react-markdown'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    padding-left: 50px;
+    margin-left: 150px;
+    margin-right: 150px;
+    
+    ul { 
+        list-style-type: none;
+    }
+    h1{ 
+        padding: 35px;
+        text-decoration: underline;
+    }
+    p { 
+        padding: 15px;
+        font-size: 18px;
+    }
+    h3 { 
+        text-decoration: underline;
+        padding: 15px
+    }
+    li { 
+        padding-left: 10px;
+    }
+    code { 
+        font-size: 15px;
+
+    }
+`
 
 export const Content = () => { 
-    const
-    return(
-        <div>Content</div>
-    )
+    const [content, setContent] = useState()
+    const {id} = useParams()
+    const location = useLocation()
+    const {type} = location.state
+
+    const fetchData = async () => { 
+        const importData = await import(`../${type}/${id}.md`)
+        const requestData = await fetch(importData.default)
+        const text = await requestData.text()
+        setContent(text)
+    }
+
+    useEffect(() => { 
+        const getData = async () => { 
+            await fetchData()
+        }
+        getData()
+
+    },[])
+
+   return(
+        <Wrapper>
+            <ReactMarkdown children={content} />
+        </Wrapper>
+   )
 }
